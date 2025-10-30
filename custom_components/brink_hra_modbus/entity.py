555c53f4736f32297c1ee58006faf0ca2fd0d41c
Jarco Vlieger <jarcovlieger@solcon.nl>
2025-10-30 16:21:08 +0100

    Improved device naming

--- custom_components/brink_hra_modbus/entity.py
+++ custom_components/brink_hra_modbus/entity.py
@@ -19,5 +19,5 @@ class BrinkEntity(CoordinatorEntity):
             identifiers={(DOMAIN, self._unique_device_id)},
             name= DEFAULT_NAME,
             manufacturer="Brink",
-            model="Flair Series",
+            model="HRA (Heat Recovery Appliance)",
     )
